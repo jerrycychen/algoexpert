a5afa3e33c66061c3745c27ab7fc954560ea509e
Jerry Chen <16475922+jerrycychen@users.noreply.github.com>
2022-02-20 22:50:27 -0800

    product sum - kotlin

---
+++ Product Sum/ProductSum.kt
@@ -0,0 +1,18 @@
+package com.algoexpert.program
+
+fun productSum(array: List<*>): Int {
+    // Write your code here.
+    return productSumHelper(array, 1)
+}
+
+fun productSumHelper(array: List<*>, multiplier: Int): Int {
+	var sum = 0
+	for (element in array) {
+		if (element is List<*>) {
+			sum += productSumHelper(element, multiplier+1)
+		} else {
+			sum += element as Int
+		}
+	}
+	return sum * multiplier
+}
