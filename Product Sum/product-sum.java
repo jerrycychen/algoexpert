d517544affa0231eb97b87cc3a9231e0e0fdbc15
Jerry Chen <16475922+jerrycychen@users.noreply.github.com>
2022-02-20 21:52:03 -0800

    product sum - java

---
+++ Product Sum/product-sum.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Program {
+    // Tip: You can use `element instanceof ArrayList` to check whether an item
+    // is an array or an integer.
+    public static int productSum(List<Object> array) {
+        // Write your code here.
+        return productSumHelper(array, 1);
+    }
+
+    public static int productSumHelper(List<Object> array, int multiplier) {
+        int sum = 0;
+        for (Object element : array) {
+            if (element instanceof List) {
+                @SuppressWarnings("unchecked")
+                List<Object> ls = (List<Object>) element;
+                sum += productSumHelper(ls, multiplier + 1);
+            } else {
+                sum += (int) element;
+            }
+        }
+        return sum * multiplier;
+    }
+}
