921c555623601f090010736a1eb662d2d809194c
Jerry Chen <16475922+jerrycychen@users.noreply.github.com>
2022-03-02 02:36:19 -0800

    depth-first search - java add

---
+++ Depth-first Search/DepthFirstSearch.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class DepthFirstSearch {
+    // Do not edit the class below except
+    // for the depthFirstSearch method.
+    // Feel free to add new properties
+    // and methods to the class.
+    static class Node {
+        String name;
+        List<Node> children = new ArrayList<Node>();
+
+        public Node(String name) {
+            this.name = name;
+        }
+
+        public List<String> depthFirstSearch(List<String> array) {
+            array.add(this.name);
+            for (int i = 0; i < children.size(); i++) {
+                children.get(i).depthFirstSearch(array);
+            }
+            return array;
+        }
+
+        public Node addChild(String name) {
+            Node child = new Node(name);
+            children.add(child);
+            return this;
+        }
+    }
+}
