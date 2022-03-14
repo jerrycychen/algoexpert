5f996f5b2721203b12d351b0a664b9b4d59213e0
Jerry Chen <16475922+jerrycychen@users.noreply.github.com>
2022-03-13 17:58:05 -0700

    remove duplicates from linked list - java add

---
+++ 013. Remove Duplicates From Linked List/RemoveDuplicatesFromLinkedList.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class RemoveDuplicatesFromLinkedList {
+    // This is an input class. Do not edit.
+    public static class LinkedList {
+        public int value;
+        public LinkedList next;
+
+        public LinkedList(int value) {
+            this.value = value;
+            this.next = null;
+        }
+    }
+
+    public LinkedList removeDuplicatesFromLinkedList(LinkedList linkedList) {
+        LinkedList currentNode = linkedList;
+        if (linkedList == null || linkedList.next == null) {
+            return linkedList;
+        }
+        while (currentNode != null) {
+            LinkedList nextDistinctNode = currentNode.next;
+            while (nextDistinctNode != null && nextDistinctNode.value == currentNode.value) {
+                nextDistinctNode = nextDistinctNode.next;
+            }
+            currentNode.next = nextDistinctNode;
+            currentNode = nextDistinctNode;
+        }
+        return linkedList;
+    }
+}
