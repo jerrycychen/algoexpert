6e4b785e6e0c72c2433eaa6390a974e75513210f
Jerry Chen <16475922+jerrycychen@users.noreply.github.com>
2022-03-23 14:45:56 -0700

    bubble sort - java add

---
+++ 018. Bubble Sort/BubbleSort.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class BubbleSort {
+    public static int[] bubbleSort(int[] array) {
+        if (array.length == 0) {
+            return array;
+        }
+        boolean isSorted = false;
+        int counter = 0;
+        while (!isSorted) {
+            isSorted = true;
+            for (int i = 0; i < array.length - 1 - counter; i++) {
+                if (array[i] > array[i + 1]) {
+                    swap(i, i + 1, array);
+                    isSorted = false;
+                }
+            }
+            counter++;
+        }
+        return array;
+    }
+
+    public static void swap(int i, int j, int[] array) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
