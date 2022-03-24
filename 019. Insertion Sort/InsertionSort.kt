2cf35d956626e60571d2bcbaaf746386b7660696
Jerry Chen <16475922+jerrycychen@users.noreply.github.com>
2022-03-24 01:14:51 -0700

    insertion sort - kotlin add

--- 019. Insertion Sort/InsertionSort.kt
+++ 019. Insertion Sort/InsertionSort.kt
@@ -1,10 +1,10 @@
 package com.algoexpert.program
 
-fun insertionSort(array: MutableList<Int>): List<Int> {
+fun selectionSort(array: MutableList<Int>): List<Int> {
     if (array.size == 0) return array
     for (i in 1 until array.size) {
         for (j in i - 1 downTo 0) {
-            if (array[j + 1] < array[j]) {
+            if (array[j] > array[j + 1]) {
                 swap(j, j + 1, array)
             }
         }
