140ebba124f37387952ddf71628fa5c290809076
Jerry Chen <16475922+jerrycychen@users.noreply.github.com>
2022-04-04 17:36:48 -0700

    palindrome check - kotlin O(n) O(n) stack add

---
+++ 021. Palindrome Check/PalindromeCheckOnOnStack.kt
@@ -0,0 +1,41 @@
+package com.algoexpert.program
+
+import java.util.ArrayDeque
+
+fun isPaliPalindromeCheckOnOnStackndrome(string: String): Boolean {
+    val length = string.length
+    if (length == 0) {
+        return false
+    } else if (length == 1) {
+        return true
+    }
+
+    var currentChar: Char
+    val isOdd: Boolean = if (length % 2 == 0) false else true
+    var stack = ArrayDeque<Char>()
+    for (i in 0 until length) {
+        if (isOdd) {
+            if (i == length / 2) {
+                // when current index is the centre pivot
+                continue
+            } else if (i < length / 2) {
+                stack.push(string.get(i))
+            } else {
+                currentChar = stack.pop()
+                if (currentChar != string.get(i)) {
+                    return false
+                }
+            }
+        } else {
+            if (i < length / 2) {
+                stack.push(string.get(i))
+            } else {
+                currentChar = stack.pop()
+                if (currentChar != string.get(i)) {
+                    return false
+                }
+            }
+        }
+    }
+    return if (stack.size == 0) true else false
+}
