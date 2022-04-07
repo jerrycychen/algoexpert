f43c5f4d43de372a0a22e1d55c43c66ae7d9f5a5
Jerry Chen <16475922+jerrycychen@users.noreply.github.com>
2022-04-07 00:53:59 -0700

    caesar cipher encryptor - java unicode add

---
+++ 022. Caesar Cipher Encryptor/CaesarCipherEncryptorUnicode.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class CaesarCipherEncryptorUnicode {
+    public static String caesarCypherEncryptor(String str, int key) {
+        char[] newLetters = new char[str.length()];
+        int newKey = key % 26;
+        for (int i = 0; i < str.length(); i++) {
+            newLetters[i] = getNewLetter(str.charAt(i), newKey);
+        }
+        return new String(newLetters);
+    }
+
+    public static char getNewLetter(char letter, int key) {
+        int newLetterCode = letter + key;
+        return newLetterCode <= 122 ? (char) newLetterCode : (char) (96 + newLetterCode % 122);
+    }
+}
