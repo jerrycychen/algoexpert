47544ab9acf120f45b41a278f5fc5d6d429887bf
Jerry Chen <16475922+jerrycychen@users.noreply.github.com>
2022-04-10 15:25:30 -0700

    three number sum - java edit

--- 026. Three Number Sum/ThreeNumberSum.java
+++ 026. Three Number Sum/ThreeNumberSum.java
@@ -2,12 +2,11 @@ import java.util.*;
 
 class ThreeNumberSum {
     public static List<Integer[]> threeNumberSum(int[] array, int targetSum) {
-        // Write your code here.
         int leftPointer;
         int rightPointer;
         int diff;
         int len = array.length;
-        List<Integer[]> res = new ArrayList<Integer[]>();
+        List<Integer[]> result = new ArrayList<Integer[]>();
 
         Arrays.sort(array); // sort the array first, O(nlogn)
         // System.out.println(Arrays.toString(array));
@@ -16,15 +15,9 @@ class ThreeNumberSum {
             leftPointer = i + 1;
             rightPointer = len - 1;
             diff = targetSum - array[i];
-            // System.out.print("diff = ");
-            // System.out.println(diff);
-            // System.out.println("-----");
             while (leftPointer < rightPointer) {
                 if (array[leftPointer] + array[rightPointer] == diff) {
-                    // System.out.printf("%d + %d == %d\n", array[leftPointer], array[rightPointer],
-                    // diff);
-                    // System.out.println("WRITE");
-                    res.add(new Integer[] { array[i], array[leftPointer], array[rightPointer] });
+                    result.add(new Integer[] { array[i], array[leftPointer], array[rightPointer] });
                     leftPointer++;
                     rightPointer--;
                 } else if (array[leftPointer] + array[rightPointer] < diff) {
@@ -34,6 +27,6 @@ class ThreeNumberSum {
                 }
             }
         }
-        return res;
+        return result;
     }
 }
