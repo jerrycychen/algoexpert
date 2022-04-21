518512c38f368708517cae7c0a8b6303695b16dd
Jerry Chen <16475922+jerrycychen@users.noreply.github.com>
2022-04-21 14:11:03 -0700

    move element to end - java add

---
+++ 028. Move Element To End/MoveElementToEnd.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Program {
+    public static List<Integer> moveElementToEnd(List<Integer> array, int toMove) {
+        int i = 0;
+        int j = array.size() - i;
+        while (i < j) {
+            while (i < j && array.get(j) == toMove)
+                j--;
+            if (array.get(i) == toMove)
+                swap(i, j, array);
+            i++;
+        }
+        return array;
+    }
+
+    public static void swap(int i, int j, List<Integer> array) {
+        int temp = array.get(j);
+        array.set(j, array.get(i));
+        array.set(i, temp);
+    }
+}
