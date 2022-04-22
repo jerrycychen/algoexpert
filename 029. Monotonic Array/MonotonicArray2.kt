662eeccf5297c4a21ee9031bcc9f519736381d31
Jerry Chen <16475922+jerrycychen@users.noreply.github.com>
2022-04-21 19:51:39 -0700

    monotonic array - kotlin add

--- 029. Monotonic Array/MonotonicArray2.kt
+++ 029. Monotonic Array/MonotonicArray2.kt
@@ -1,23 +1,11 @@
 package com.algoexpert.program
 
 fun isMonotonic(array: List<Int>): Boolean {
-    if (array.size <= 2) return true
-
-    var direction = array[1] - array[0]
-    for (i in 2 until array.size) {
-        if (direction == 0) {
-            direction = array[i] - array[i - 1]
-            continue
-        }
-        if (breaksDirection(direction, array[i - 1], array[i])) {
-            return false
-        }
+    var isNonDecreasing = true
+    var isNonIncreasing = true
+    for (i in 1 until array.size) {
+        if (array[i] < array[i - 1]) isNonDecreasing = false
+        if (array[i] > array[i - 1]) isNonIncreasing = false
     }
-    return true
-}
-
-fun breaksDirection(direction: Int, previousInt: Int, currentInt: Int): Boolean {
-    val difference = currentInt - previousInt
-    if (direction > 0) return difference < 0
-    return difference > 0
+    return isNonIncreasing || isNonDecreasing
 }
