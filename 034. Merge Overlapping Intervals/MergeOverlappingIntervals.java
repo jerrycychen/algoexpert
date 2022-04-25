d2bdb0b97522f820970e5e8f8867ca89f7753e86
Jerry Chen <16475922+jerrycychen@users.noreply.github.com>
2022-04-24 21:40:51 -0700

    merge overlapping intervals - java add

---
+++ 034. Merge Overlapping Intervals/MergeOverlappingIntervals.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Program {
+
+    // O(nlog(n)) time | O(n) space - where n is the length of the input array
+    public static int[][] mergeOverlappingIntervals(int[][] intervals) {
+        // Sort the intervals by starting value.
+        int[][] sortedIntervals = intervals.clone();
+        Arrays.sort(sortedIntervals, (a, b) -> Integer.compare(a[0], b[0]));
+
+        List<int[]> mergedIntervals = new ArrayList<int[]>();
+        int[] currentInterval = sortedIntervals[0];
+        mergedIntervals.add(currentInterval);
+
+        for (int[] nextInterval : sortedIntervals) {
+            int currentIntervalEnd = currentInterval[1];
+            int nextIntervalStart = nextInterval[0];
+            int nextIntervalEnd = nextInterval[1];
+
+            if (currentIntervalEnd >= nextIntervalStart) {
+                currentInterval[1] = Math.max(currentIntervalEnd, nextIntervalEnd);
+            } else {
+                currentInterval = nextInterval;
+                mergedIntervals.add(currentInterval);
+            }
+        }
+        return mergedIntervals.toArray(new int[mergedIntervals.size()][]);
+    }
+}
